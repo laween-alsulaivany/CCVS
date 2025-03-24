302e760c855c970186a8a9019a1f72f0dc9c1dc6
Judah Nava <judahnava02@gmail.com>
2025-03-23 20:08:37 -0500

    - The scheduler module, has issues, the apscheduler is not a module inside it. 	- [ ] Could this be a version issue or a limitation imposed by IT? - Other then that, code seems to be running smoothly. - Added install instructions to readme

--- src/main.py
+++ src/main.py
@@ -3,9 +3,19 @@ This is the main entry point for the CCVS Data Persistence and GitHub Integratio
 This module is responsible for saving and loading the game state to and from a local file, as well as committing the game state to a GitHub repository.
 """
 
+""" This is from 
+kb8125mc@smoke:~$ chess
+Traceback (most recent call last):
+  File "/home/remote/kb8125mc/CCVS/src/main.py", line 8, in <module>
+    import scheduler
+  File "/home/remote/kb8125mc/CCVS/src/scheduler.py", line 12, in <module>
+    from apscheduler.schedulers.background import BackgroundScheduler
+ModuleNotFoundError: No module named 'apscheduler'
+"""
+
 
 # from src.data_persistence import save_game_state, load_game_state
-import scheduler
+# import scheduler
 
 
 def main():
