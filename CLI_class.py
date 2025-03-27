29a5e4dc68eb3932c4e9d7b21c09ebe65122fd25
applesauce3535 <bjohnson03123@gmail.com>
2025-03-26 22:46:38 -0500

    refactored CLI_class.py, added game.py, which replaced board_and_pieces.py

--- CLI_class.py
+++ CLI_class.py
@@ -1,14 +1,17 @@
-
+import chess
 
 class CLI:
+    player = "Ben"
+    played_games = 0
+    turn_number = 0
+    wins = 0
     def __init__(self):
-        pass
+        pass    
+    
+    def displayCLI(self, board):
+        print( f"Number of games {self.player} has played: {self.played_games}" \
+               f"{' ' * 11}Turn number: {self.turn_number}\n" \
+               f"Number of games {self.player} has won: {self.wins}\n" \
+               f"Captured pieces:\n" \
+               f"Example on how to play: <initial space><target space> e.g. e2e3") # implement playable moves and decide on how to play the game
 
-    def displayCLI(self):
-        print(f"Number of games {self.player} has played: {self.played_games}"
-              f"{' ' * 11}Turn number: {self.turn_number}\n"
-              f"Number of games {self.player} has won: {self.wins}"
-              f"{' ' * 14}Playable moves:\n"
-              f"Fallen pieces:\n"
-              # implement playable moves and decide on how to play the game
-              f"Example on how to play:")
