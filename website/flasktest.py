79b16198fe1cb7d407a32ccb75964677a5af9c07
donovanmorse <vanmorse17@gmail.com>
2025-04-10 17:12:02 -0500

    start of actuall web

--- website/flasktest.py
+++ website/flasktest.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Hello! Welcome to CSIS Chess!</h1><h3>Put /'your_name' at the end of the url to see something cool!</h3>"
+
+@app.route("/<name>")
+def user(name):
+    dons = ["Donovan", "Donnie", "Donny"]
+    if name in dons:
+        return f"{name} is awesome!"
+    else:
+        return f"{name} is a bitch!"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=80)
