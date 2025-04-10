8741865481f4475d04e549f62cebb52b198895a1
donovanmorse <vanmorse17@gmail.com>
2025-04-10 17:13:34 -0500

    small update

--- website/web_main.py
+++ website/web_main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    return ""
+    return render_template("index.html", test_sentance="I'm a variable")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=80)
