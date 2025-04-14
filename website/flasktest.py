18fbdc9f61dad9a4f5f5664236905c6a97158c9d
donovanmorse <vanmorse17@gmail.com>
2025-04-14 10:15:45 -0500

    add comments

--- website/flasktest.py
+++ website/flasktest.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#sets the address
 @app.route("/")
 
+#the page itself
 def index():
     return "<h1>Hello! Welcome to CSIS Chess!</h1><h3>Put /'your_name' at the end of the url to see something cool!</h3>"
 
+#different page
 @app.route("/<name>")
 def user(name):
     dons = ["Donovan", "Donnie", "Donny"]
@@ -15,5 +18,6 @@ def user(name):
     else:
         return f"{name} is a bitch!"
 
+#sets the ip
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=80)
