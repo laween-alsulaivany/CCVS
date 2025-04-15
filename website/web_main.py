bbe912882878f52702e9df26f3a4c69f4cd64c82
Donovan Morse <vanmorse17@gmail.com>
2025-04-15 15:49:23 -0500

    skeleton attempt 2

--- website/web_main.py
+++ website/web_main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html", test_sentance="I'm a variable")
+    if request.method == "POST":
+        return redirect(url_for("chess"))
+    else:
+        return render_template("index.html" )
+
+@app.route("/chess")
+def chess():
+    return render_template("chess.html", output = "This is where the board will be displayed. \n This is where the board will be displayed. \n This is where the board will be displayed.".replace('\n', '<br>'), color = "White")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=80)
